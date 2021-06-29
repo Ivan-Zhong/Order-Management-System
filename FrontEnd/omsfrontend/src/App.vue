3393c091037e84d77dcbb53803d6acd9e8267773
Jerome_Tang <853555867@qq.com>
2021-06-29 15:13:10 +0800

    modify

--- FrontEnd/omsfrontend/src/App.vue
+++ FrontEnd/omsfrontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Login</router-link> |
+    <router-link to="/logout">Logout</router-link>
   </div>
   <router-view/>
 </template>
